ff54fbcc36a3ad5a7e53b74d0d62414a92759121
fthkynr06 <89317693+fthkynr06@users.noreply.github.com>
2021-12-09 23:02:47 +0300

    3

--- src/main/java/Github.java
+++ src/main/java/Github.java
@@ -2,6 +2,7 @@ public class Github {
     public static void main(String[] args) {
         System.out.println("Merhaba Dunya");
         System.out.println("selam");
+        System.out.println("deneme");
 
     }
 }
